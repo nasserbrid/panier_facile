479a467cec5b1ced186bfdcd83e0b0f100b1bc69
Nasser <nasserbrid@gmail.com>
2025-09-16 10:48:19 +0200

    Mise en place de l'architecture de l'application, authentication, core, panier, templates, mise en place de models dans authentication, de forms et de validators, ajouté de l'app panier et authentication dans settings.py

---
+++ authentication/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    ADMIN = 'ADMIN'
+    USER = 'USER'
+    
+    ROLE_CHOICES = (
+        (ADMIN, 'Administrateur'),
+        (USER, 'Utilisateur'),
+    )
+    
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
