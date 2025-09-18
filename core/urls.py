54ba0c84fad57f1a8338d8a5a47c5494819bab89
Nasser <nasserbrid@gmail.com>
2025-09-18 10:06:56 +0200

    ajout des urls pour les templates de panier ajouter_panier_au_course.html etc..

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,43 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
+import authentication.views
+import panier.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(
+        template_name= 'authentication/login.html',
+        redirect_authenticated_user = True
+    ), name='login'),
+      path('logout/', authentication.views.CustomLogoutView.as_view(), name='logout'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name = "authentication/password_change_form.html"), name='password_change'),
+    #path('change-password-done/', PasswordChangeDoneView.as_view(template_name = "authentication/password_change_done.html"), name='password_change_done'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(
+        template_name="authentication/password_reset_done.html"), name="password_reset_done"),
+    #path('password_reset/', PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset/', PasswordResetView.as_view(
+        template_name="authentication/password_reset_form.html"), name="password_reset"),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name="authentication/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset/done/', PasswordResetCompleteView.as_view(
+        template_name="authentication/password_reset_complete.html"), name="password_reset_complete"),
+    path('signup/', authentication.views.signup_page, name='signup'),
+    
+    path('creer_course/', panier.views.creer_course, name='creer_course'),
+    path('courses/', panier.views.liste_courses, name='liste_courses'),
+    path('courses/<int:course_id>/', panier.views.detail_course, name='detail_course'),
+    path('courses/<int:course_id>/ajouter_ingredient/', panier.views.ajouter_ingredient, name='ajouter_ingredient'),
+    path('courses/<int:course_id>/modifier/', panier.views.modifier_course, name='modifier_course'),
+    path('home/', panier.views.home, name='home'),
+    path('', RedirectView.as_view(url='/home/'), name='index'),
+   
+    
+    
 ]
