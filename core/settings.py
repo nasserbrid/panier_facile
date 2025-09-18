e88ecb949b77747957f5c3038ff9ac888b81e44b
Nasser <nasserbrid@gmail.com>
2025-09-18 07:53:13 +0200

    mise en place de views.py pour panier

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'PanierFacile',
         'USER': 'postgres',
-        'PASSWORD': 'Nasser12121987',
+        'PASSWORD': 'Nasser12121987*',
         'HOST': 'localhost',
         'PORT': '5432',
         
