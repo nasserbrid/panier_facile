82c3a8d129496a6560c1d3274153b0a9f807e0dc
Nasser <nasserbrid@gmail.com>
2025-09-20 15:51:24 +0200

    mise à jour de settings.py et ci.yml

--- core/settings.py
+++ core/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'PanierFacile',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': 'localhost',
-        'PORT': '5432',
+         'NAME': os.getenv("DB_NAME", "PanierFacile"),
+        'USER': os.getenv("DB_USER", "postgres"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT': os.getenv("DB_PORT", "5432"),
         
         },
     }
