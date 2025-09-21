1f06fb695b3e729147b37848985b7cb94d11166c
Nasser <nasserbrid@gmail.com>
2025-09-21 15:05:51 +0200

    Ajout d'un dossier static pour le css et le js, amélioration du visuelle pour detail_panier et detail_course

--- core/settings.py
+++ core/settings.py
@@ -158,6 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
